aed78c630dbd13e052aee590fb62831633de96c3
rdeioris <roberto.deioris@gmail.com>
2023-12-06 12:59:52 +0100

    added Extras directory

---
+++ Extras/glTFRuntimeVoxUE4ColorSpaceLibrary.h
@@ -0,0 +1,13 @@
+// Copyright 2023, Roberto De Ioris.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "glTFRuntimeVoxUE4ColorSpaceLibrary.generated.h"
+
+UENUM()
+enum class ETextureColorSpace : uint8
+{
+	Unsupported
+};
