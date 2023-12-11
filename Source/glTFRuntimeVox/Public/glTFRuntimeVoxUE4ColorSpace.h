e37988d64fe5da9f6f8181a65d0993b9f9872c26
rdeioris <roberto.deioris@gmail.com>
2023-12-11 10:18:54 +0100

    fixed ue5

--- Source/glTFRuntimeVox/Public/glTFRuntimeVoxUE4ColorSpace.h
+++ Source/glTFRuntimeVox/Public/glTFRuntimeVoxUE4ColorSpace.h
@@ -1,12 +1,3 @@
 // Copyright 2023, Roberto De Ioris.
 
 #pragma once
-
-#include "CoreMinimal.h"
-#include "glTFRuntimeVoxUE4ColorSpace.generated.h"
-
-UENUM()
-enum class ETextureColorSpace : uint8
-{
-	Unsupported
-};
